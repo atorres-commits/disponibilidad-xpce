e3035fc68b3a4b9406a1b7ce841cdc54b53c579b
atorres-commits <atorres@xpce.es>
2026-09-23 10:42:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const URBAN_DATA = require("./urban_data.json");
-
+const MALAGA_DATA = require("./malaga_data.json");
 const PORT = process.env.PORT || 10000;
 
 const ALOJAMIENTOS = {
