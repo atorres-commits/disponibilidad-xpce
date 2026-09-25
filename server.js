2c3df0efe732e76fffcddc1d74a1b86e08aeb202
atorres-commits <atorres@xpce.es>
2026-09-25 09:23:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -96,7 +96,31 @@ if (url.pathname === "/solicitud-reserva") {
 
   return await procesarSolicitudReserva(req, res);
 }
+// --------------------------------------------------
+// ENVIAR INCIDENCIA DE HUESPED
+// --------------------------------------------------
 
+if (url.pathname === "/enviar-incidencia") {
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    });
+
+    return res.end();
+  }
+
+  if (req.method !== "POST") {
+    return enviarJSON(res, 405, {
+      ok: false,
+      error: "Metodo no permitido. Utiliza POST."
+    });
+  }
+
+  return await procesarIncidencia(req, res);
+}
         // --------------------------------------------------
     // INFORMACION METEOROLOGICA
     // --------------------------------------------------
@@ -1250,31 +1274,7 @@ function buscarAlojamientoPorNombre(nombre) {
     );
   });
 }
-// --------------------------------------------------
-// ENVIAR INCIDENCIA DE HUESPED
-// --------------------------------------------------
 
-if (url.pathname === "/enviar-incidencia") {
-
-  if (req.method === "OPTIONS") {
-    res.writeHead(204, {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type"
-    });
-
-    return res.end();
-  }
-
-  if (req.method !== "POST") {
-    return enviarJSON(res, 405, {
-      ok: false,
-      error: "Metodo no permitido. Utiliza POST."
-    });
-  }
-
-  return await procesarIncidencia(req, res);
-}
 // --------------------------------------------------
 // INFORMACION METEOROLOGICA
 // --------------------------------------------------
